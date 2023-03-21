fd94e7020c06a779a9bb70bd35b60a4f1d72e46f
François Blackburn <fblackburn@sram.qc.ca>
2023-03-21 07:48:03 -0400

    Add bufferline

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+-- bufferline.lua
+--
+-- Add bufferline plugin
+
+vim.keymap.set('n', '<left>', vim.cmd.bn)
+vim.keymap.set('n', '<right>', vim.cmd.bp)
+
+return {
+	'akinsho/bufferline.nvim',
+	dependencies = {
+		'nvim-tree/nvim-web-devicons'
+	},
+	config = function ()
+		require('bufferline').setup {}
+	end
+}

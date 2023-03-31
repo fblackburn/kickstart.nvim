0e01d7c4c5d6172d39094ae38a9536f19c579cf6
François Blackburn <fblackburn@sram.qc.ca>
2023-03-31 14:59:01 -0400

    Add keybinds to terminal toggle

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -4,6 +4,14 @@
 
 vim.opt.hidden = true
 
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 return {
 	'akinsho/toggleterm.nvim',
 	opts = {
